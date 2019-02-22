a1b22ca9b142cac56d3f8914f0ee4f7a677c3e3a
Vladislav Pomogaev <comsa44@gmail.com>
2019-02-21 20:05:14 -0800

    Added vector math library. Added function for calculating acceleration due to gravity. Added basic rocket parameters

--- src/rocketsimulator/Rocket.java
+++ src/rocketsimulator/Rocket.java
@@ -1,24 +1,46 @@
 package rocketsimulator;
 
-import java.util.Stack;
-import java.util.Vector;
+import java.util.ArrayList;
+import javax.vecmath.*;
 
 public class Rocket {
 	
-	Stack<RocketComponent> RocketParts;
-	Vector<Double> position; //x y z coordinates in meters
-	Vector<Double> orientation; //angles of the rocket (to be determined)
-	Vector<Double> torque; //x y z coordinates in newton-meters
-	Vector<Double> force; //x y z components in newtons
+	public static final Double G = 6.673E-11; //gravitational constant
+	public static final Double r = 6378000.0; //radius of the earth in meters
+	public static final Double m = 5.972E24; //mass of earth in kg
+	
+	ArrayList<RocketComponent> RocketParts;
+	Vector3d position; //vector from starting position to CG of rocket
+	Vector3d orientation; //orientation of rocket (see https://www.grc.nasa.gov/www/k-12/rocket/rotations.html)
+	Vector3d torque; //newton-meters around CG
+	Vector3d force; //newtons through CG
 
 	Rocket() {
-		RocketParts = new Stack<RocketComponent>();
-		position = new Vector<Double>();
-		orientation = new Vector<Double>();
+		RocketParts = new ArrayList<RocketComponent>();
+		position = new Vector3d();
+		orientation = new Vector3d();
 	}
 	
-	public void calculateForceOfGravity() {
-		
+	/*
+	 * Returns the force vector on the rocket due to gravity acting through the center of mass
+	 */
+	public Vector3d calculateForceOfGravity() {
+		Double rocket_mass = 0.0;
+		//for each component of the 
+		for (RocketComponent comp : RocketParts) {
+			rocket_mass = rocket_mass + comp.getMass();
+		}
+		Double force = rocket_mass * getAcceleration();
+		Vector3d down = new Vector3d(0.0,0.0,-force);
+		return down;
+	}
+	
+	/*
+	 * Returns the acceleration due to gravity of the rocket based on the altutute
+	 */
+	public Double getAcceleration() {
+		//formula: a = G * Mearth / r^2
+		return G * m / Math.pow((r + position.getZ()), 2);
 	}
 	
 	public void addComponent(RocketComponent comp) {
